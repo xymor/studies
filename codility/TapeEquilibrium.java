d3d8f490140995679a3dc8dd6c7905b3840818f8
xymor <raphaelmiranda@gmail.com>
2015-09-29 20:01:33 -0300

    Update TapeEquilibrium.java

--- codility/TapeEquilibrium.java
+++ codility/TapeEquilibrium.java
@@ -15,3 +15,33 @@ public int equilibrium(int[] array) {
 	}
 	return list.size() - 1 + position;
 }
+
+V2
+
+import java.util.stream.*;
+
+class Solution {
+    public int solution(int[] A ){
+          return minDiff(A);
+    }
+    public static int minDiff(int[] a) {
+        int leftSum=0, rightSum=0;
+        leftSum = IntStream.of(a).sum();
+        
+        int minDiff = Integer.MAX_VALUE;
+        for(int p = a.length -1; p >= 0; p--){
+            rightSum += a[p];
+            leftSum -= a[p];
+            
+            int diff = Math.abs(leftSum - rightSum);
+            if(diff == 0){
+                return 0;
+            } else if(diff < minDiff){
+                minDiff = diff;
+            }
+        }
+        return minDiff;
+    }
+}
+
+
